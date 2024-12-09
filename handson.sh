de5968596504cc2b9cc127d7ceb0937bced4ad0e
kevwe-20 <kevwe_oberiko@yahoo.com>
2024-12-09 11:14:22 -0500

    Updated the handson.sh file

--- handson.sh
+++ handson.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-echo_ "Creating Directory"
+echo "Creating Directory"
 mkdir user-apps2
-echo_ "Finished creating Directory"
+echo "Finished creating Directory"
 
-echo_ "Creating files"
-touch user-apps/web-app.txt
-touch user-apps/nginx.cfg
-touch user-apps/readme.txt
-touch user-apps/install.sh
-echo_ "Finished creating all four files"
+echo "Creating files"
+touch user-apps2/web-app.txt
+touch user-apps2/nginx.cfg
+touch user-apps2/readme.txt
+touch user-apps2/install.sh
+echo "Finished creating all four files"
 
-sudo adduser my-test-user2
+cp -p user-apps2/* .
+echo "Finished copying all four files"
+
+sudo adduser --disabled-password --gecos "" my-test-user2
+# sudo adduser my-test-user2
+echo "my-test-user2:password" | sudo chpasswd
 sudo chown my-test-user2 web-app.txt
 
-echo_ "We are loged in as $USER"
+echo "We are loged in as $USER"
 message="We are running from a script"
 files=`ls`
 
 
-echo_ $message
-echo_ "The files in the Directory are $files"
-
+echo $message
+echo "The files in the Directory are $files"
